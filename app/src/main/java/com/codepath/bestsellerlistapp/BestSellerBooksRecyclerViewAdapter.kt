418836dd51e5d208836b26df6866333875f4ff96
Isaiah Freeman <ikfreeman@yahoo.com>
2023-02-14 15:13:51 -0500

    Initial commit

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
@@ -3,8 +3,11 @@ package com.codepath.bestsellerlistapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.codepath.bestsellerlistapp.R.id
 
 /**
@@ -31,7 +34,10 @@ class BestSellerBooksRecyclerViewAdapter(
         var mItem: BestSellerBook? = null
         val mBookTitle: TextView = mView.findViewById<View>(id.book_title) as TextView
         val mBookAuthor: TextView = mView.findViewById<View>(id.book_author) as TextView
-
+        val mBookImage : ImageView = mView.findViewById<ImageView>(id.book_image)
+        val mBookRanking: TextView = mView.findViewById<View>(id.ranking) as TextView
+        val mDescription: TextView = mView.findViewById<View>(id.book_description) as TextView
+        val mBuyButton :Button = mView.findViewById<Button>(id.buy_button)
         override fun toString(): String {
             return mBookTitle.toString() + " '" + mBookAuthor.text + "'"
         }
@@ -46,6 +52,12 @@ class BestSellerBooksRecyclerViewAdapter(
         holder.mItem = book
         holder.mBookTitle.text = book.title
         holder.mBookAuthor.text = book.author
+        holder.mDescription.text = book.description
+        holder.mBookRanking.text = book.rank.toString()
+        Glide.with(holder.mView)
+            .load(book.bookImageUrl)
+            .centerInside()
+            .into(holder.mBookImage)
 
         holder.mView.setOnClickListener {
             holder.mItem?.let { book ->
